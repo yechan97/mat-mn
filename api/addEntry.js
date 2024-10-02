528ec12fa01e8e04f7aec5a2460b61ec8a099880
yechan97 <kirsty8317@gmail.com>
2024-10-03 06:13:41 +0900

    Update addEntry.js

--- api/addEntry.js
+++ api/addEntry.js
@@ -1,26 +1,32 @@
-// api/addEntry.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Firebase 콘솔에서 이 파일을 다운로드해야 합니다.
+const cors = require('cors')({ origin: true });
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
   });
 }
 
 const db = admin.firestore();
 
-module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    const entry = req.body;
+module.exports = (req, res) => {
+  cors(req, res, async () => {
+    if (req.method === 'POST') {
+      const entry = req.body;
 
-    try {
-      await db.collection('entries').add(entry);
-      res.status(200).json({ message: 'Entry added successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'Error adding entry' });
+      try {
+        await db.collection('entries').add(entry);
+        res.status(200).json({ message: 'Entry added successfully' });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error adding entry' });
+      }
+    } else {
+      res.status(405).json({ error: 'Method not allowed' });
     }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
+  });
 };
