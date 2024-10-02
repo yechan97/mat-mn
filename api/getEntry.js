d6dc8672be19c9bb223fe9d739562cd597168082
yechan97 <kirsty8317@gmail.com>
2024-10-03 06:13:49 +0900

    Update getEntry.js

--- api/getEntry.js
+++ api/getEntry.js
@@ -1,25 +1,31 @@
-// api/getEntries.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // 동일한 파일을 사용합니다.
+const cors = require('cors')({ origin: true });
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
   });
 }
 
 const db = admin.firestore();
 
-module.exports = async (req, res) => {
-  if (req.method === 'GET') {
-    try {
-      const snapshot = await db.collection('entries').get();
-      const entries = snapshot.docs.map(doc => doc.data());
-      res.status(200).json(entries);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching entries' });
+module.exports = (req, res) => {
+  cors(req, res, async () => {
+    if (req.method === 'GET') {
+      try {
+        const snapshot = await db.collection('entries').get();
+        const entries = snapshot.docs.map(doc => doc.data());
+        res.status(200).json(entries);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error fetching entries' });
+      }
+    } else {
+      res.status(405).json({ error: 'Method not allowed' });
     }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
+  });
 };
